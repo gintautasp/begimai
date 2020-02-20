8389b425a31823af85a7dba38ca4930bac734d41
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2020-02-20 14:14:53 +0200

    veikia Haversine

--- put-to-db.php
+++ put-to-db.php
@@ -8,13 +8,30 @@
 <body>
 <?php
 
+	$pi = 4 * atan ( 1.0 );
+	echo "pi: " . $pi . "<br>";
+
+	function point_in_rad ( $pt_in_grad ) {
+		
+		global $pi;
+	
+		$pt_in_rad  = new stdClass;
+		$koef = (  $pi / 180 );
+		
+		$pt_in_rad -> lat = ( float ) $pt_in_grad [ 'lat' ] * $koef;
+		$pt_in_rad -> lon = ( float ) $pt_in_grad [ 'lon' ] * $koef;
+		$pt_in_rad -> time = $pt_in_grad -> time;
+		
+		return $pt_in_rad;
+	}
+
 	$gpx = simplexml_load_file( "20200216_095948.gpx" );
 	
 	// https://lt.geofumadas.com/kompleksiniai-scenarijai-for-skai%C4%8Diavimai/
 	// https://lt.wikipedia.org/wiki/Radianas
 	// https://en.wikipedia.org/wiki/Haversine_formula
 	
-	$r = 6371; // �emes spindulys, km 
+	$r = 6371; // žemės spindulys, km 
 
 	foreach ( $gpx -> trk as $trk ) {
 	
@@ -24,55 +41,43 @@
 <?php
 
 			$pt_prev_x = ( ( array ) $seg -> trkpt [ 0 ] ) [ '@attributes' ];
-			// $pt_prev [ 'lat' ] = $seg -> trkpt [ 0 ] [ 'lat' ];
-			// $pt_prev [ 'lon' ] = $seg -> trkpt [ 0 ] [ 'lon' ];
 
-			
-			// print_r ( $pt_prev [ 'lat' ] [ 0 ]  );
-			// print_r ( $pt_prev [ 'lon' ] [ 0 ] ); 
-			print_r ( $pt_prev_x /* [ '@attributes' ] */ );
-			$pt_prev = array();
-			$pt = array();
-			$pt_prev [ 'lat' ] = ( float ) $pt_prev_x [ 'lat' ];
-			echo ( $pt_prev [ 'lat' ] );
-			$pt_prev [ 'lon' ] = ( float ) $pt_prev_x [ 'lon' ];
-			echo ( $pt_prev [ 'lon' ] );			
+			print_r ( $pt_prev_x  );
+
+			$pt_prev = point_in_rad ( $pt_prev_x );
 			
 			$flag_count = true;
 			$sum_dist = 0;
 	
 			foreach ( $seg -> trkpt as $pt_x ) {
-			
-				$pt [ 'lat' ] = ( float ) $pt_x [ 'lat' ];
-				$pt [ 'lon' ] = ( float ) $pt_x [ 'lon' ];
-			
+
+				$pt =  point_in_rad ( $pt_x );
+				
 				if ( $flag_count ) {
 			
 					$dist 
 						= 
-								acos ( 
+								asin ( 
 								
 									sqrt (
-											pow ( sin ( ( $pt [ 'lat' ] - $pt_prev [ 'lat' ] ) / 2 ), 2 ) 
+											pow ( sin ( ( $pt -> lat - $pt_prev -> lat ) / 2 ), 2 ) 
 										+ 	
-											cos ( $pt_prev [ 'lat' ] )
+											cos ( $pt_prev -> lat )
 										* 
-											cos ( $pt [ 'lat' ] ) 
+											cos ( $pt -> lat ) 
 										*
-											pow ( sin ( ( $pt [ 'lon' ] - $pt_prev [ 'lon' ] ) / 2 ), 2 )
+											pow ( sin ( ( $pt -> lon - $pt_prev -> lon ) / 2 ), 2 )
 									)
 								) 
 							* 
-								( 2 *  $r )
+								( 2 *  $r * 1000)
 						;
 					$sum_dist += $dist;
-						
-					// $flag_count = false;
 				}
 ?>			
 				<tr>
-					<td><?= $pt [ 'lat' ] ?></td>
-					<td><?= $pt [ 'lon' ] ?></td>
+					<td><?= $pt -> lat ?></td>
+					<td><?= $pt -> lon ?></td>
 					<td><?= $dist  ?></td>
 					<td><?= $pt_x -> time  ?></td>					
 				</tr>
