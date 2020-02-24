a21140f54299b19e15f1b1b05be1f905918a55c9
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2020-02-24 14:29:11 +0200

    kazko naujo nepridariau

--- put-to-db.php
+++ put-to-db.php
@@ -8,21 +8,29 @@
 <body>
 <?php
 
-	$pi = 4 * atan ( 1.0 );
+	$pi = 4 * atan ( 1.0 );	
 	echo "pi: " . $pi . "<br>";
+	$koef_grad_to_rad = (  $pi / 180 );
 
-	function point_in_rad ( $pt_in_grad ) {
+	function mk_point_x ( $pt_gpx ) {
 		
-		global $pi;
+		global $pi, $koef_grad_to_rad;
 	
 		$pt_in_rad  = new stdClass;
-		$koef = (  $pi / 180 );
-		
-		$pt_in_rad -> lat = ( float ) $pt_in_grad [ 'lat' ] * $koef;
-		$pt_in_rad -> lon = ( float ) $pt_in_grad [ 'lon' ] * $koef;
-		$pt_in_rad -> time = $pt_in_grad -> time;
+
+		$pt_in_rad -> lat_grad = $pt_gpx [ 'lat' ];
+		$pt_in_rad -> lon_grad = $pt_gpx [ 'lon' ];
+		$pt_in_rad -> lat = ( float ) $pt_gpx [ 'lat' ] * $koef_grad_to_rad;
+		$pt_in_rad -> lon = ( float ) $pt_gpx [ 'lon' ] * $koef_grad_to_rad;
+		$pt_in_rad -> time = $pt_gpx -> time;
 		
 		return $pt_in_rad;
+	}
+	
+	function mk_point_z ( $pt_from, $pt_to ) {
+	
+		
+		
 	}
 
 	$gpx = simplexml_load_file( "20200216_095948.gpx" );
@@ -37,21 +45,21 @@
 	
 		foreach ( $trk -> trkseg as $seg ) {
 ?>
-			<table><tr><th>lat</th><th>lon</th></tr>
+			<table><tr><th>lat</th><th>lon</th><th>dist</th><th>time</th></tr>
 <?php
 
-			$pt_prev_x = ( ( array ) $seg -> trkpt [ 0 ] ) [ '@attributes' ];
+			$pt_prev_x = ( ( array ) $seg ->   trkpt [ 0 ] ) [ '@attributes' ];
 
 			print_r ( $pt_prev_x  );
 
-			$pt_prev = point_in_rad ( $pt_prev_x );
+			$pt_prev = mk_point_x ( $pt_prev_x );
 			
 			$flag_count = true;
 			$sum_dist = 0;
 	
 			foreach ( $seg -> trkpt as $pt_x ) {
 
-				$pt =  point_in_rad ( $pt_x );
+				$pt =  mk_point_x ( $pt_x );
 				
 				if ( $flag_count ) {
 			
@@ -70,7 +78,7 @@
 									)
 								) 
 							* 
-								( 2 *  $r * 1000)
+								( 2 *  $r * 1000 )
 						;
 					$sum_dist += $dist;
 				}
